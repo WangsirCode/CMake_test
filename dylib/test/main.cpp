94ccf958e5bef0b25a9a85de6b951ef2c7fd7f5e
bryant.wang <bryant.wang@dji.com>
2018-08-24 12:29:12 +0800

    init

---
+++ dylib/test/main.cpp
@@ -0,0 +1,10 @@
+#include "sample.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout<< "Factorial(5) results: "<<Factorial(5)<< endl;
+  cout<< "IsPrime(5) results: "<<IsPrime(5)<< endl;
+}
